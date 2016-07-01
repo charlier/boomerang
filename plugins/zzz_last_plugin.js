b94f24508f8553d8d6dbd8d4c3b9a4d60bcbd1a9
Charlie Rogers <charlier@users.noreply.github.com>
2016-07-01 10:54:40 +0100

    add basic page type

--- plugins/zzz_last_plugin.js
+++ plugins/zzz_last_plugin.js
@@ -1,1 +1,6 @@
 BOOMR.t_end = new Date().getTime();
+
+BOOMR.addVar('page_type', document.getElementsByTagName('BODY')[0].getAttribute('data-page-type'));
+BOOMR.init({
+    beacon_url: '/beacon'
+});
